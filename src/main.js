dc32d2b2d34bf59d50b8878336e19b4f5cbe21cd
Jay <joaomarcelo1015ja@gmail.com>
2024-03-16 00:46:24 -0300

    Novas seções e ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
-    
+    const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero')
+    const altHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const altAtual = window.scrollY
+        const header = document.querySelector('.header')
+
+        if (altAtual < altHero){
+            header.classList.add('header--is-hidden')
+        }
+        else{
+            header.classList.remove('header--is-hidden')
+        }
+    })
+
+
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(btn){
             const tabTarget = btn.target.dataset.tabButton;
@@ -12,7 +29,18 @@ document.addEventListener('DOMContentLoaded', function(){
             btn.target.classList.add('shows__tabs__buttons--is-active')
         })
     }
+
+    for(let i=0; i < questions.length; i++){
+        questions[i].addEventListener('click', openClose)
+    }
 })
+
+function openClose(element){
+    const classe = 'faq__questions__item--is-open'
+    const qstPai = element.target.parentNode
+    qstPai.classList.toggle(classe)
+
+}
 
 function hideAll(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]')
